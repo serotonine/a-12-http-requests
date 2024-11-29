0fa74fc8a24a4cdad145117e0526c7f69f04a23f
serotonine <ks.serotonine@gmail.com>
2024-11-29 14:35:37 +0100

    01.httpClient.get

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# a-12-http-requests
+# ANGULAR Http Requests.
+### Repository
+https://github.com/serotonine/a-12-http-requests
+
+### About
+Build [Maximilian Schwarzmüller](https://www.udemy.com/user/maximilian-schwarzmuller) Http Requests: See below links.
+
+***
+
+### ANGULAR 18
+**xxx**
+
+##### Summary
+- [Angular HttpClient + provideHttpClient](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44116304)
+- [Add HttpClient in NgModule](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44127430)
+
--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,11 @@
-import { Component, signal } from '@angular/core';
-
-import { Place } from '../place.model';
+import { Component, signal, inject, OnInit, DestroyRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
+// Components.
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+// Interfaces.
+import { type Place } from '../place.model';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +14,34 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  // Http Client Service injection.
+  private _httpClient = inject(HttpClient);
+  destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    // Return an Observable.
+    const subscription = this._httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        // Return the entire response datas.
+        observe: 'response',
+      })
+      // Transforms data if needed.
+      .pipe(
+        map((response) => response.body?.places),
+        // Optional.
+        catchError((error) =>
+          throwError(() => new Error('Couille in ze potage'))
+        )
+      )
+      .subscribe({
+        next: (response) => this.places.set(response),
+        complete: () => this.isFetching.set(false),
+        error: (error: Error) => this.error.set(error.message),
+      });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
