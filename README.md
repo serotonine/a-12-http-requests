49514e134fd58b14732d57a8225fffff25438bd9
serotonine <ks.serotonine@gmail.com>
2024-12-01 19:36:14 +0100

    Introducing HTTP Interceptors.

--- README.md
+++ README.md
@@ -15,4 +15,14 @@ Build [Maximilian Schwarzmüller](https://www.udemy.com/user/maximilian-schwarzm
 - [Add HttpClient in NgModule](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44127430)
 - [Move http request logic in a Service](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44116338)
 - [`tap`](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44116340)
-
+-[Introducing HTTP Interceptors](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44116366)
+  httpClient has methods which allow you to register a so-called Interceptor.
+  Register an Interceptor at the place you provide the HTTP CLIent : here in the Bootstrap app (Main.ts).
+  ```js
+    bootstrapApplication(AppComponent, {
+      providers:[provideHttpClient(
+        withInterceptors([])
+      )]
+    }).catch((err) => console.error(err));
+  ```
+  in the array, pass functions.
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
+import { tap } from 'rxjs';
 
-
+function loggingInterceptor(request:HttpRequest<unknown>, next:HttpHandlerFn){
+  console.log('/// Outgoing request ////');
+  console.log(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if(event.type === HttpEventType.Response){
+          console.log('/// Incoming Response ////');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      }
+    })
+  );
+}
 
 bootstrapApplication(AppComponent, {
-  providers:[provideHttpClient()]
+  providers:[provideHttpClient(
+    withInterceptors([loggingInterceptor])
+  )]
 }).catch((err) => console.error(err));
